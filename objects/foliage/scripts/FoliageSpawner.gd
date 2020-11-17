166a5c6ef5efeea83b27c7c7dd2e17dacbf53dd8
Bryan Miller <bmiller1008@gmail.com>
2020-11-17 13:57:05 -0500

    Foliage sprites scaled up. Flower object broken. Added 'Foliage Spawner' that currently only spawns grass.

---
+++ objects/foliage/scripts/FoliageSpawner.gd
@@ -0,0 +1,192 @@
+tool
+extends Node2D
+
+const GRASS_WIDTH = 32.0
+
+var grass = preload("res://objects/foliage/Grass.tscn")
+
+enum ANCHOR {
+	LEFT,
+	CENTER,
+	RIGHT
+}
+
+export (float, 32.0, 3200.0) var area = 128 setget _set_area
+export (ANCHOR) var anchor = ANCHOR.CENTER setget _set_anchor
+export (float, 0.0, 32.0) var bg_y_offset setget _set_bg_y_offset
+export (float, 0.0, 32.0) var fg_y_offset setget _set_fg_y_offset
+export var bg_container_path : NodePath = ""
+export var fg_container_path : NodePath = ""
+export (float, 0.0, 1.0) var grow_rate = 0.2
+export (float, 0.1, 1.0) var spread = 1.0
+export (float, 0.0, 1.0) var variance = 0.0
+export var grow_on_start : bool = false
+
+var bg_container = null
+var fg_container = null
+
+var grass_list = []
+var grass_to_spawn = 0
+var grass_dist = 0
+var spawn_count = 0
+var last_l_offset = 0
+var last_r_offset = 0
+var last_spawn_side = 0 # 0 = center | 1 = right | -1 = left
+
+func _set_area(a: float):
+	if a != area:
+		area = clamp(a, 32.0, 3200.0)
+		update()
+
+func _set_bg_y_offset(off : float):
+	off = clamp(off, 0.0, 32.0)
+	if off != bg_y_offset:
+		bg_y_offset = off
+		update()
+
+func _set_fg_y_offset(off : float):
+	off = clamp(off, 0.0, 32.0)
+	if off != fg_y_offset:
+		fg_y_offset = off
+		update()
+
+func _set_anchor(a):
+	if a != anchor:
+		anchor = a
+		update()
+
+
+func _ready():
+	set_process(false)
+	bg_container = get_node(bg_container_path)
+	fg_container = get_node(fg_container_path)
+	grass_dist = GRASS_WIDTH * spread
+	grass_to_spawn = floor(area / grass_dist)
+	update()
+	if grow_on_start:
+		grow()
+
+
+func grow():
+	if not Engine.editor_hint:
+		set_process(true)
+
+func _process(delta):
+	if not Engine.editor_hint:
+		if grass_list.size() >= (grass_to_spawn * 2):
+			return
+
+		spawn_count += delta * grow_rate * grass_to_spawn
+		print(spawn_count)
+		if spawn_count < 1.0:
+			return
+		
+		# Only keep the fraction
+		var count = floor(spawn_count)
+		spawn_count -= count
+		
+		match(anchor):
+			ANCHOR.LEFT:
+				_spawnLeftgrass(count)
+			ANCHOR.RIGHT:
+				_spawnRightGrass(count)
+			ANCHOR.CENTER:
+				_spawnCenterGrass(count)
+
+
+func variant_dist():
+	if variance == 0:
+		return grass_dist
+	return grass_dist - (grass_dist * rand_range(0.0, variance))
+
+func _spawnLeftgrass(count):
+	for i in range(0, count):
+		if grass_list.size() <= 0:
+			_spawn(global_position)
+		else:
+			last_l_offset += variant_dist()
+			var pos = global_position
+			pos.x -= last_l_offset
+			_spawn(pos)
+
+func _spawnRightGrass(count):
+	for i in range(0, count):
+		if grass_list.size() <= 0:
+			_spawn(global_position)
+		else:
+			last_r_offset += variant_dist()
+			var pos = global_position
+			pos.x += last_r_offset
+			_spawn(pos)
+			
+
+func _spawnCenterGrass(count):
+	for i in range(0, count):
+		var off = 0.0
+		if last_spawn_side < 0:
+			last_l_offset -= variant_dist()
+			off = last_l_offset
+		elif last_spawn_side > 0:
+			last_r_offset += variant_dist()
+			off = last_r_offset
+		else:
+			last_spawn_side = 1
+		last_spawn_side *= -1
+			
+		var pos = global_position
+		pos.x += off
+		_spawn(pos)
+
+
+func _spawn(pos):
+	if bg_container != null:
+		var g = grass.instance()
+		g.bloomed = false
+		g.start_growing = true
+		g.position.x = pos.x
+		g.position.y = pos.y - bg_y_offset
+		bg_container.add_child(g)
+		grass_list.append(g)
+	if fg_container != null:
+		var g = grass.instance()
+		g.bloomed = false
+		g.start_growing = true
+		g.position.x = pos.x
+		g.position.y = pos.y + fg_y_offset
+		fg_container.add_child(g)
+		grass_list.append(g)
+
+
+func _draw():
+	if Engine.editor_hint:
+		var guide_color = Color(0.25, 1.0, 0.0)
+		var offset_color = Color(0.0, 0.25, 1.0)
+		var r1 = null
+		var r2 = null
+		match(anchor):
+			ANCHOR.CENTER:
+				var h = area * 0.5
+				r1 = Rect2(-h, -4.0, h - 16.0, 8.0)
+				r2 = Rect2(16, -4, h - 16.0, 8)
+			ANCHOR.LEFT:
+				r1 = Rect2(-area, -4.0, area - 16.0, 8.0)
+			ANCHOR.RIGHT:
+				r1 = Rect2(16.0, -4.0, area - 16.0, 8.0)
+		draw_rect(Rect2(-16, -16, 32.0, 32.0), guide_color, false, 2.0)
+		
+		draw_rect(r1, guide_color, true)
+		r1.position.y = -bg_y_offset
+		r1.size.y = 4.0
+		draw_rect(r1, offset_color, true)
+		r1.position.y = fg_y_offset
+		draw_rect(r1, offset_color, true)
+		if r2 != null:
+			draw_rect(r2, guide_color, true)
+			r2.position.y = -bg_y_offset
+			r2.size.y = 4.0
+			draw_rect(r2, offset_color, true)
+			r2.position.y = fg_y_offset
+			draw_rect(r2, offset_color, true)
+
+
+
--- objects/foliage/scripts/Grass.gd
+++ objects/foliage/scripts/Grass.gd
@@ -6,6 +6,8 @@ export var grow_time : float = 2.0
 export var bloomed : bool = true setget _set_bloomed
 export var start_growing : bool = false
 
+export(float, 0.0, 1.0) var wind_variance : float = 0.2 setget _set_wind_variance
+
 
 var ready = false
 var grown = false
@@ -21,21 +23,33 @@ func _set_bloomed(b : bool):
 	bloomed = b
 	if ready:
 		if bloomed:
-			$cluster.scale.x = 1.0
+			$cluster.scale.y = 1.0
 		else:
-			$cluster.scale.x = 0.0
+			$cluster.scale.y = 0.0
+
+
+func _set_wind_variance(v : float, force : bool = false):
+	v = clamp(v, 0.0, 1.0)
+	if v != wind_variance or force:
+		wind_variance = v
+		if ready:
+			var mat = $cluster.get_material()
+			mat.set_shader_param("strengthScale", 50 + (rand_range(-(50 * wind_variance), 50 * wind_variance)))
+			mat.set_shader_param("minStrength", 0.1 + rand_range(0.0, 0.1 * wind_variance))
+			mat.set_shader_param("maxStrength", 0.2 + rand_range(0.0, 0.2 * wind_variance))
 
 func _ready():
 	ready = true
+	_set_color(color, true)
+	_set_wind_variance(wind_variance, true)
 	_set_bloomed(bloomed)
 	if not bloomed and start_growing and not Engine.editor_hint:
 		grow()
 
-
 func grow():
 	if not grown:
 		grown = true
-		$cluster.scale.x
-		$Tween.interpolate_property($cluster, "scale.x", $cluster.scale.x, 1.0, grow_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+		$cluster.scale.y
+		$Tween.interpolate_property($cluster, "scale:y", $cluster.scale.y, 1.0, grow_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 		$Tween.start()
 
