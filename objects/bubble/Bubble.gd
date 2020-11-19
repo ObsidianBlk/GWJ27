fb07fe043f05fae86992620bbdf274cb677d44d6
Bryan Miller <bmiller1008@gmail.com>
2020-11-18 19:57:03 -0500

    Tweaked lighting. Added 'parallax background'. Fixed dialog box. Added bubbles (but broken)

---
+++ objects/bubble/Bubble.gd
@@ -0,0 +1,52 @@
+extends Node2D
+
+
+export (float, 0.0, 200.0) var wave_length = 10.0
+export (float, 0.0, 1.0) var wave_length_variance = 0.0
+export (float, 0.1, 60.0) var wave_duration = 1.0
+export (float, 0.1, 60.0) var life_time = 4.0
+export (float, 0.0, 1.0) var life_variance = 0.0
+export (float, 0.1, 10000.0) var lift_per_second = 2.0
+
+
+var cur_life_time = 0.0
+var cur_life = 0.0
+var cur_wave_length = 0.0
+
+func _calc_variance(base, variance):
+	return base + (base * (rand_range(-variance, variance)))
+
+func _ready():
+	cur_life_time = _calc_variance(life_time, life_variance)
+	cur_wave_length = _calc_variance(wave_length, wave_length_variance)
+	$Tween_Death.connect("tween_all_completed", self, "_on_dead")
+	$Tween_Wave.connect("tween_all_completed", self, "_on_wave_shift")
+	
+	$Tween_Wave.interpolate_property($Viz, "position:x", $Viz.position.x, cur_wave_length * 0.5, wave_duration * 0.25, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	$Tween_Wave.start()
+	
+	var lift_duration = cur_life_time + (cur_life_time * 0.5)
+	var lift_distance = lift_per_second * lift_duration
+	$Tween_Lift.interpolate_property(self, "global_position:y", self.global_position.y, self.global_position.y - lift_distance, lift_duration,Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	$Tween_Lift.start()
+
+func _physics_process(delta):
+	if cur_life < 0:
+		return
+
+	cur_life += delta
+	if cur_life >= cur_life_time:
+		$Tween_Death.interpolate_property($Viz, "modulate", $Viz.modulate, Color(1.0, 1.0, 1.0, 0.0), cur_life_time * 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+		$Tween_Death.interpolate_property($Viz, "scale", $Viz.scale, Vector2(0.0, 0.0), cur_life_time * 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+		$Tween_Death.start()
+
+
+func _on_wave_shift():
+	var target = (cur_wave_length * 0.5)
+	if $Viz.position.x > 0:
+		target = -(cur_wave_length * 0.5)
+	$Tween_Wave.interpolate_property($Viz, "position:x", $Viz.position.x, target, wave_duration * 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	$Tween_Wave.start()
+
+func _on_dead():
+	self.queue_free()
--- objects/foliage/scripts/FoliageSpawner.gd
+++ objects/foliage/scripts/FoliageSpawner.gd
@@ -1,9 +1,11 @@
 tool
 extends Node2D
 
+const LIGHT_BOX_BASE_WIDTH = 32.0
 const GRASS_WIDTH = 32.0
 
 var grass = preload("res://objects/foliage/Grass.tscn")
+var bubble = preload("res://objects/bubble/Bubble.tscn")
 
 enum ANCHOR {
 	LEFT,
@@ -21,6 +23,8 @@ export (float, 0.0, 1.0) var grow_rate = 0.2
 export (float, 0.1, 1.0) var spread = 1.0
 export (float, 0.0, 1.0) var variance = 0.0
 export var grow_on_start : bool = false
+export var grass_color : Color = Color(0.25, 1.0, 0.0, 1.0)
+export (float, 0.0, 100.0) var bubbles_per_second = 4.0
 
 var bg_container = null
 var fg_container = null
@@ -33,6 +37,17 @@ var last_l_offset = 0
 var last_r_offset = 0
 var last_spawn_side = 0 # 0 = center | 1 = right | -1 = left
 
+var light_max_x_scale = 0.0
+
+var bubble_spawn_count = 0
+
+
+func _calc_light_y_scale():
+	# NOTE... Light box is square, so it's width and height are the same.
+	# So ignore the "_WIDTH" part.
+	var off_dist = (LIGHT_BOX_BASE_WIDTH * 2) + bg_y_offset + fg_y_offset + GRASS_WIDTH
+	$Light.scale.y = off_dist / (LIGHT_BOX_BASE_WIDTH * 2)
+
 func _set_area(a: float):
 	if a != area:
 		area = clamp(a, 32.0, 3200.0)
@@ -42,17 +57,29 @@ func _set_bg_y_offset(off : float):
 	off = clamp(off, 0.0, 32.0)
 	if off != bg_y_offset:
 		bg_y_offset = off
+		_calc_light_y_scale()
 		update()
 
 func _set_fg_y_offset(off : float):
 	off = clamp(off, 0.0, 32.0)
 	if off != fg_y_offset:
 		fg_y_offset = off
+		_calc_light_y_scale()
 		update()
 
 func _set_anchor(a):
 	if a != anchor:
 		anchor = a
+		match(anchor):
+			ANCHOR.LEFT:
+				$Light.offset.x = -(LIGHT_BOX_BASE_WIDTH * 0.5)
+				$Light.position.x = (LIGHT_BOX_BASE_WIDTH * 0.5)
+			ANCHOR.RIGHT:
+				$Light.offset.x = (LIGHT_BOX_BASE_WIDTH * 0.5)
+				$Light.position.x = -(LIGHT_BOX_BASE_WIDTH * 0.5)
+			ANCHOR.CENTER:
+				$Light.offset.x = 0
+				$Light.position.x = 0
 		update()
 
 
@@ -62,6 +89,9 @@ func _ready():
 	fg_container = get_node(fg_container_path)
 	grass_dist = GRASS_WIDTH * spread
 	grass_to_spawn = floor(area / grass_dist)
+	
+	light_max_x_scale = (area / LIGHT_BOX_BASE_WIDTH) + 1
+	
 	update()
 	if grow_on_start:
 		grow()
@@ -69,64 +99,100 @@ func _ready():
 
 func grow():
 	if not Engine.editor_hint:
+		var gps = grow_rate * grass_to_spawn
+		var duration = grass_to_spawn / gps
+		$Light.enabled = true
+		$Tween.interpolate_property($Light, "scale:x", $Light.scale.x, light_max_x_scale, duration, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+		$Tween.start()
 		set_process(true)
 
 func _process(delta):
 	if not Engine.editor_hint:
-		if grass_list.size() >= (grass_to_spawn * 2):
-			return
+		if grass_list.size() < (grass_to_spawn * 2):
+			spawn_count += delta * grow_rate * grass_to_spawn
+			if spawn_count < 1.0:
+				return
+			
+			# Only keep the fraction
+			var count = floor(spawn_count)
+			spawn_count -= count
+			
+			match(anchor):
+				ANCHOR.LEFT:
+					_spawnDirectional(count, -1)
+				ANCHOR.RIGHT:
+					_spawnDirectional(count, 1)
+				ANCHOR.CENTER:
+					_spawnCenterGrass(count)
 
-		spawn_count += delta * grow_rate * grass_to_spawn
-		if spawn_count < 1.0:
-			return
-		
-		# Only keep the fraction
-		var count = floor(spawn_count)
-		spawn_count -= count
-		
-		match(anchor):
-			ANCHOR.LEFT:
-				_spawnLeftgrass(count)
-			ANCHOR.RIGHT:
-				_spawnRightGrass(count)
-			ANCHOR.CENTER:
-				_spawnCenterGrass(count)
+		bubble_spawn_count += delta * bubbles_per_second
+		if bubble_spawn_count >= 1.0:
+			var count = floor(bubble_spawn_count)
+			bubble_spawn_count -= count
+			
+			for i in range(0.0, count):
+				_spawn_bubble()
+
+
+func _spawn_bubble():
+	if bg_container == null:
+		return
+	
+	var b = bubble.instance()
+	var posx = global_position.x
+	match(anchor):
+		ANCHOR.LEFT:
+			posx -= rand_range(0.0, area)
+		ANCHOR.RIGHT:
+			posx += rand_range(0.0, area)
+		ANCHOR.CENTER:
+			posx += rand_range(-(area*0.5), area*0.5)
+	b.position.x = posx
+	b.position.y = global_position.y - bg_y_offset
+	bg_container.add_child(b)
+	
 
 
 func variant_dist():
 	if variance == 0:
 		return grass_dist
-	return grass_dist - (grass_dist * rand_range(0.0, variance))
+	return grass_dist + ((grass_dist*0.5) * rand_range(0.0, variance))
 
-func _spawnLeftgrass(count):
+
+func _spawnDirectional(count, dir):
 	for i in range(0, count):
+		if last_l_offset >= area:
+			grass_to_spawn = grass_list.size() * 0.5
+			return
+
 		if grass_list.size() <= 0:
 			_spawn(global_position)
 		else:
-			last_l_offset += variant_dist()
+			var dist = variant_dist()
+			last_l_offset += dist
+			if last_l_offset > area:
+				last_l_offset = area
 			var pos = global_position
-			pos.x -= last_l_offset
+			pos.x += dir * last_l_offset
 			_spawn(pos)
-
-func _spawnRightGrass(count):
-	for i in range(0, count):
-		if grass_list.size() <= 0:
-			_spawn(global_position)
-		else:
-			last_r_offset += variant_dist()
-			var pos = global_position
-			pos.x += last_r_offset
-			_spawn(pos)
-			
 
 func _spawnCenterGrass(count):
+	var harea = area * 0.5
 	for i in range(0, count):
 		var off = 0.0
+		if last_l_offset <= -harea or last_r_offset >= harea:
+			grass_to_spawn = grass_list.size() * 0.5
+			return
+
 		if last_spawn_side < 0:
 			last_l_offset -= variant_dist()
+			if last_l_offset < -harea:
+				last_l_offset = -harea
 			off = last_l_offset
 		elif last_spawn_side > 0:
 			last_r_offset += variant_dist()
+			if last_r_offset > harea:
+				last_r_offset = harea
 			off = last_r_offset
 		else:
 			last_spawn_side = 1
@@ -139,21 +205,19 @@ func _spawnCenterGrass(count):
 
 func _spawn(pos):
 	if bg_container != null:
-		var g = grass.instance()
-		g.bloomed = false
-		g.start_growing = true
-		g.position.x = pos.x
-		g.position.y = pos.y - bg_y_offset
-		bg_container.add_child(g)
-		grass_list.append(g)
+		_createGrassIn(bg_container, Vector2(pos.x, pos.y - bg_y_offset))
 	if fg_container != null:
-		var g = grass.instance()
-		g.bloomed = false
-		g.start_growing = true
-		g.position.x = pos.x
-		g.position.y = pos.y + fg_y_offset
-		fg_container.add_child(g)
-		grass_list.append(g)
+		_createGrassIn(fg_container, Vector2(pos.x, pos.y + fg_y_offset))
+
+
+func _createGrassIn(container, pos):
+	var g = grass.instance()
+	g.bloomed = false
+	g.start_growing = true
+	g.color = grass_color
+	g.position = pos
+	container.add_child(g)
+	grass_list.append(g)
 
 
 func _draw():
